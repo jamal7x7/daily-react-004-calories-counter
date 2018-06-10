619d3e4d3e4c259d69df4c11a127a8eb36c67f50
jamal7X7 <jamal7x7@gmail.com>
2018-06-10 05:29:41 +0000

    flexbox war

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ daily-react-004-calories-counter
 
 Challenge: create a Calories counter app.
 
+check my submission: https://github.com/jamal7x7/daily-react-004-calories-counter …
+
 #100DaysOfReact #100DaysOfCode #dailyReact #javascript #react #reactjs
--- src/components/CaloriesCounterApp.js
+++ src/components/CaloriesCounterApp.js
@@ -5,18 +5,51 @@ import '../styles/CaloriesCounterApp.css'
 const Header = (props) => (
   <header className='App-header'>
     {/* <img src={logo} className='App-logo' alt='logo' /> */}
-    <h1 className='App-title'>Welcome to React</h1>
+    <h1 className='App-title'>Calories Counter</h1>
   </header>
 )
 
-const Container = (props) => (
-  <div className='container'>
-    <div className='mainWin'>
-      hello
+const MainWin = (props) => (
+  <div className='mainWin'>
+    <div className='left'>
+
+      <div className='category-title'>
+        <p className='meals-cat-title'>
+          Meals
+        </p>
+        <p className='calories-cat-title'>
+          Calories
+        </p>
+      </div>
+
+      <div className='meal'>
+        <p className='meal-type'> breakfast </p>
+        <div className='table'>
+          <div className='food'>
+            <p>Orange jus</p>
+            <p>bread</p>
+          </div>
+
+          <div className='bar'>
+            475 cal
+          </div>
+        </div>
+      </div>
+
     </div>
-    <div className='btn'>
-      +
+
+    <div className='graph'>
+
+        Calories
+
     </div>
+
+  </div>
+)
+
+const Btn = (props) => (
+  <div className='btn'>
+    +
   </div>
 )
 
@@ -25,7 +58,10 @@ class CaloriesCounterApp extends Component {
     return (
       <div className='App'>
         <Header />
-        <Container />
+        <div className='container'>
+          <MainWin />
+          <Btn />
+        </div>
       </div>
     )
   }
