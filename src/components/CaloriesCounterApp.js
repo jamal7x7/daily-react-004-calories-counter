1261be74fdf5a700619f9c40de3f1d19e251340f
jamal7X7 <jamal7x7@gmail.com>
2018-06-10 15:15:41 +0000

    modal added

--- src/components/CaloriesCounterApp.js
+++ src/components/CaloriesCounterApp.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 import logo from '../logo.svg'
 import '../styles/CaloriesCounterApp.css'
+import Modal from 'react-modal'
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 const Header = (props) => (
   <header className='App-header'>
@@ -48,19 +60,63 @@ const MainWin = (props) => (
 )
 
 const Btn = (props) => (
-  <div className='btn'>
+  <div className='btn' onClick={props.openModal}>
     +
   </div>
 )
 
+
+
 class CaloriesCounterApp extends Component {
+
+  state = {
+    modalIsOpen: false
+  };
+
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
+
   render () {
     return (
       <div className='App'>
         <Header />
         <div className='container'>
           <MainWin />
-          <Btn />
+          <Btn openModal={this.openModal}/>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+            className="Modal"
+            overlayClassName="Overlay"
+            >
+            <div className="modal">
+            <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+            <button onClick={this.closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+            
+            </div>
+
+          </Modal>
         </div>
       </div>
     )
